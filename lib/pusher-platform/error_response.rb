b615e8b602c7327994f2e1367ec4a1109d971585
Paweł Ledwoń <me@ludvik.pl>
2017-01-28 11:13:42 +0000

    Change error body to description

--- lib/pusher-platform/error_response.rb
+++ lib/pusher-platform/error_response.rb
@@ -1,15 +1,15 @@
 module Pusher
   class ErrorResponse < Error
-    attr_accessor :status, :headers, :body
+    attr_accessor :status, :headers, :description
 
-    def initialize(status, headers, body)
+    def initialize(status, headers, description)
       @status = status
       @headers = headers
-      @body = body
+      @description = description
     end
 
     def to_s
-      "Pusher::ErrorResponse: #{status} #{body}"
+      "Pusher::ErrorResponse: #{status} #{description}"
     end
   end
 end
