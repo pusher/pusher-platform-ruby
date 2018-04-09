c6970f75f310a3b8d51034fe68caaec06d0389e8
Hamilton Chapman <hamchapman@gmail.com>
2018-04-09 17:41:01 +0100

    Move and update server example

---
+++ auth_server_example/sample.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+require 'json'
+require 'cgi'
+require_relative '../lib/pusher-platform'
+
+# Get these from the Dashbaord
+instance = PuPusherPlatformsher::Instance.new(
+  locator: 'v1:api-ceres:some-instance-id',
+  key: 'key-id:key-secret',
+  service_name: 'chatkit',
+  service_version: 'v1'
+)
+
+post '/pusherplatform/authenticate' do
+  instance.authenticate_with_request(request, { user_id: 'ham' })
+end
